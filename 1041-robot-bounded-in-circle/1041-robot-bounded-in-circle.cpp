562694dab2b00f0d07c1330ce735aca01b3b8739
Hariom Mewada <56331816+hariom575@users.noreply.github.com>
2022-01-10 12:38:07 +0530

    Time: 0 ms (100.00%), Space: 6 MB (94.23%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -1,60 +1,47 @@
 class Solution {
-    public:
-    int max_x, max_y, min_x, min_y;
-    int x, y;
-    bool is_vertical;
-    bool is_positive;
-
-    void update() {
-        max_x = max(max_x, x);
-        min_x = min(min_x, x);
-        max_y = max(max_y, y);
-        min_y = min(min_y, y);
+public:
+    //returns true if the co-ordinates are at origin
+    bool originVerifier(int x,int y){
+        if(x == 0 && y == 0)
+			return true;
+		return false;
     }
-    void traverse(const string &s) {
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == 'G') {
-                if (is_vertical)
-                    (is_positive) ? y++ : y--;
-                else
-                    (is_positive) ? x++ : x--;
-                update();
-            } else if (s[i] == 'L') {
-                if (is_vertical)
-                    (is_positive) ? (is_vertical = is_positive = false)
-                    : (is_vertical = false, is_positive = true);
-                else
-                    (is_positive) ? (is_vertical = is_positive = true)
-                    : (is_vertical = true, is_positive = false);
-
-            } else {
-                if (is_vertical)
-                    (is_positive) ? (is_vertical = false, is_positive = true)
-                    : (is_vertical = false, is_positive = false);
-                else
-                    (is_positive) ? (is_vertical = true, is_positive = false)
-                    : (is_vertical = true, is_positive = true);
+	
+    bool isRobotBounded(string instructions) {
+        //given
+        char current_dir = 'N';
+        //co-ordinate points
+        int x = 0, y= 0;
+        for(int i=0;i<instructions.length();i++){ 
+            //Case1: If 'G' is in the instruction string
+            if(instructions[i] == 'G'){
+                y += current_dir =='N' ? 1 : 0;
+				y += current_dir =='S' ? -1 : 0;
+				x += current_dir == 'E' ? 1 : 0;
+				x += current_dir == 'W' ? -1 : 0;
             }
+            //case2: If we found 'directions'
+            else{
+                char incoming = instructions[i];
+                if(current_dir == 'N'){
+                    current_dir = incoming == 'L' ? 'W' : 'E';
+                }
+                else if(current_dir == 'W'){
+                    current_dir = incoming == 'L' ? 'S' : 'N';
+                }
+                else if(current_dir == 'S'){
+                    current_dir = incoming == 'L' ? 'E' : 'W';
+                }
+                else{
+                    current_dir = incoming == 'L' ? 'N' : 'S';
+                }
+            } 
+            
         }
+        //if the robo is at origin or the current direction is not north than return true
+        if(originVerifier(x,y) || current_dir!='N')
+            return true;
+        
+        return false;
     }
-    bool isRobotBounded(string s) {
-        max_x = INT_MIN, max_y = INT_MIN, min_x = INT_MAX, min_y = INT_MAX;
-        x = 0, y = 0;
-        is_vertical = false;
-        is_positive = true;
-
-        for (int i = 0; i <= s.size(); i++)
-            traverse(s);
-
-        int curr_max_x = max_x, curr_mx_y = max_y;
-        int curr_min_x = min_x, curr_mn_y = min_y;
-
-        for (int i = 0; i < s.size(); i++)
-            traverse(s);
-
-        return (curr_max_x == max_x and curr_mx_y == max_y and
-                curr_min_x == min_x and curr_mn_y == min_y)
-            ? true
-            : false;
-    }
-} ob;
+};
