7a1fbeb09d9f18c7579bfa66ca44855cf5bf215f
Hariom Mewada <56331816+hariom575@users.noreply.github.com>
2022-01-26 17:33:47 +0530

    Time: 120 ms (96.62%), Space: 84.5 MB (79.25%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -9,23 +9,45 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution
-{
+class Solution {
 public:
-     vector<int> res;
-     void dfs(TreeNode *root)
-     {
-          if (!root)
-               return;
-          res.push_back(root->val);
-          dfs(root->left);
-          dfs(root->right);
-     }
-     vector<int> getAllElements(TreeNode *root1, TreeNode *root2)
-     {
-          dfs(root1);
-          dfs(root2);
-          sort(begin(res), end(res));
-          return res;
-     }
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        stack<TreeNode*>s1, s2; //stack for inorder traversal
+        vector<int>ans;
+        if(root1 == nullptr && root2 == nullptr)
+            return ans;
+        TreeNode *r1 = root1, *r2 = root2;
+        int x, y;
+        while(!s1.empty() || !s2.empty() || r1 != nullptr || r2 != nullptr)
+        {
+            while(r1 != nullptr)
+            {
+                s1.push(r1);
+                r1 = r1->left;
+            }
+            while(r2 != nullptr)
+            {
+                s2.push(r2);
+                r2 = r2->left;
+            }
+			// top of the stack will have smallest node currently not processed in the BST
+            x = (!s1.empty()) ? s1.top()->val : INT_MAX;
+            y = (!s2.empty()) ? s2.top()->val : INT_MAX;
+            //cout<<x<<" "<<y<<"\n";
+			// Now compare values and merge while traversing
+            if(x < y)
+            {
+                ans.push_back(x);
+                r1 = s1.top()->right;
+                s1.pop();
+            }
+            else
+            {
+                ans.push_back(y);
+                r2 = s2.top()->right;
+                s2.pop();
+            }
+        }
+        return ans;
+    }
 };
