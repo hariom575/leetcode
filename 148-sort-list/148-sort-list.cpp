12f479af09816b5c7359064fd59af3b2767447fe
Hariom Mewada <56331816+hariom575@users.noreply.github.com>
2022-02-24 21:47:34 +0530

    Time: 181 ms (28.04%), Space: 48.7 MB (24.25%) - LeetHub

---
+++ 148-sort-list/148-sort-list.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+     if(head==NULL || head->next==NULL) return head;
+        ListNode * temp=NULL;
+        ListNode * slow=head;
+        ListNode *fast=head;
+        while(fast && fast->next){
+            temp=slow;
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        temp->next=NULL;
+        ListNode *l1=sortList(head);
+        ListNode *l2 =sortList(slow);
+        return mergeList(l1,l2);
+    }
+    ListNode* mergeList(ListNode * l1 ,ListNode *l2){
+        ListNode *ptr= new ListNode(0);
+        ListNode *cur=ptr;
+        while(l1 && l2){
+            if(l1->val<=l2->val){
+                cur->next=l1;
+                l1=l1->next;
+            }
+             else{
+                cur->next=l2;
+                l2=l2->next;
+            }
+            cur=cur->next;
+        }
+        if(l1)
+            cur->next=l1;
+        if(l2)
+            cur->next=l2;
+        return ptr->next;
+    }
+};
