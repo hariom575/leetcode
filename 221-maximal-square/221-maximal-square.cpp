4476da13dda9582e438ed73dcfd969d00bc96f53
Hariom Mewada <56331816+hariom575@users.noreply.github.com>
2022-08-14 21:49:22 +0530

    Time: 106 ms (49.67%), Space: 17.2 MB (74.38%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& matrix) {
-        int n=matrix.size();
-         int m=matrix[0].size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0 || j==0) {dp[i][j]=(matrix[i][j]=='1');continue;}
-                if(matrix[i][j]=='1'){
-                    dp[i][j]=min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]))+1;
-                }
-            }
-        }
+    int vis[301][301];
+    int dp[301][301];
+    int res=0;
+    int helper(int row,int col,int n,int m,vector<vector<char>>&matrix){
+        if(row>=n || col>=m || row<0 || col<0) return 0;
+        if(dp[row][col]!=-1) return dp[row][col];
+        int ans1=helper(row+1,col,n,m,matrix);
+        int ans2=helper(row,col+1,n,m,matrix);
+        int ans3=helper(row+1,col+1,n,m,matrix);
         int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++) ans=max(ans,dp[i][j]);
-          //  cout<<endl;
+        if(matrix[row][col]=='1'){
+            ans=min(ans1,min(ans2,ans3))+1;
         }
-        return ans*ans;
+        res=max(res,ans*ans);
+        return dp[row][col]=ans;
+    }
+    int maximalSquare(vector<vector<char>>& matrix) {
+        memset(dp,-1,sizeof(dp));
+        helper(0,0,matrix.size(),matrix[0].size(),matrix);
+        return res;
     }
 };
