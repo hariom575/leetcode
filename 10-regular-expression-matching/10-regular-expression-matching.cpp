7a87766fcb6170ce9cb5a677b251aa0b4d11ee90
Hariom Mewada <56331816+hariom575@users.noreply.github.com>
2022-08-14 19:26:21 +0530

    Time: 7 ms (68.23%), Space: 7.1 MB (50.13%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,49 +1,23 @@
 class Solution {
 public:
-    map<int, map<int, bool>>dp;
-    
-    bool solve(string str1, string str2, int index1, int index2){
-        
-        if(index1 >= str1.length() && index2 >= str2.length()){
-            return true;
-        }
-        
-        if(index1>=str1.length()){
-            if((str2.length() - index2)%2!=0)
-                return false;
-            while(index2 <= str2.length()-2){
-                if(str2[index2+1] == '*')
-                    index2+=2;
-                else
-                    return false;
-            }
-            return true;
-        }
-        
-        if(index2>=str2.length()){
-            return false;
-        }
-        
-        if(dp[index1].find(index2)!=dp[index1].end())
-            return dp[index1][index2];
-        
-        bool res = false;
-        
-        if(index2+1 < str2.length() && str2[index2+1] == '*'){
-            if(str1[index1] == str2[index2] || str2[index2] == '.'){
-                res = res || solve(str1, str2, index1+1, index2) || solve(str1, str2, index1, index2+2);
-            }   
-            else{
-                res = res || solve(str1, str2, index1, index2+2);
-            }
-        }
-        if(str1[index1] == str2[index2] || str2[index2] == '.') 
-            res = res || solve(str1, str2, index1+1, index2+1);
-        
-        return dp[index1][index2] = res;
-    }
-    
     bool isMatch(string s, string p) {
-        return solve(s, p, 0, 0);
+        int n=s.size();
+        int m=p.size();
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,0));
+        dp[0][0]=1;
+        for(int i=1;i<=m;i++){
+            if(p[i-1]=='*') dp[0][i]=dp[0][i-2];
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='.'){
+                    dp[i][j]=dp[i-1][j-1];
+                }else if(p[j-1]=='*'){
+                    dp[i][j]=dp[i][j-2];
+                    if(p[j-2]=='.'  || p[j-2]==s[i-1]) dp[i][j]=dp[i][j] | dp[i-1][j];
+                }else dp[i][j]=false;
+            }
+        }
+        return dp[n][m];
     }
 };
